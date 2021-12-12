f1cc85ee17469fbec54c2c9a4dfa3e1ca1003e57
MehmedKhan <msalah.96@outlook.com>
2021-12-12 13:53:31 +0200

    Bootloader with python script

---
+++ PY SCRIPT/main.py
@@ -0,0 +1,45 @@
+# This is a sample Python script.
+import serial
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    com = input("Enter your Com Port Number : ")
+
+    ser = serial.Serial(
+        port='COM' + str(com),
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=15)
+
+    file = open("NVIC.hex", "r")
+    rec = 1
+
+    while True:
+        line = file.readline()
+        line = line.replace(':', '', -1)
+
+        for x in range(len(line)):
+            char = line[x]
+            ser.write(char.encode())
+
+        print('Sent Record No :: ' + str(rec))
+        rec = rec + 1
+        data = ser.read(1)
+
+        if data == b'D':
+            continue
+        if data == b'F':
+            break
+        if data == b'E':
+            print('RECORD CORRUPTION ...')
+            break
+
+    ser.close()
+
+    print('Flashing Is Done...')
+
+
+
